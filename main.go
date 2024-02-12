eabda026606ac831e97841019d63e8e24c55bf11
droidy12527 <gundiaramasu@gmail.com>
2024-02-12 21:00:00 +0000

    features: added tokens for tokenization in the language, Added more generic tokens but impl of some has been yet to be made

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"go/parser"
-	"go/token"
 	"os"
 	"strings"
 )
@@ -34,27 +30,28 @@ func readFile() {
 	if fileExtension != "pookie" {
 		panic("file extension is wrong, make sure you are using pookie file extension.")
 	}
-	file, err := os.Open(filename)
-	if err != nil {
-		panic("file not found, missing file.")
-	}
-	var words []string
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanBytes)
-	for scanner.Scan() {
-		words = append(words, scanner.Text())
-	}
-	fmt.Println(words)
-	// lexer.Lex(words)
-	tokens := token.NewFileSet()
-	parsingFile, err := parser.ParseFile(tokens, "./lexicalstack/lexicalstack.go`", nil, parser.Trace)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// Create AST (Abstract Syntax Tree) For The Module Declaration and then convert into Bytecode for more flexibilty
-	fmt.Println(*parsingFile)
-	// for i := 0; i < len(parsingFile.Imports); i++ {
-	// 	fmt.Println(*parsingFile.Imports[i])
+
+	// file, err := os.Open(filename)
+	// if err != nil {
+	// 	panic("file not found, missing file.")
 	// }
-	fmt.Println(tokens)
+	// var words []string
+	// scanner := bufio.NewScanner(file)
+	// scanner.Split(bufio.ScanBytes)
+	// for scanner.Scan() {
+	// 	words = append(words, scanner.Text())
+	// }
+	// fmt.Println(words)
+	// // lexer.Lex(words)
+	// tokens := token.NewFileSet()
+	// parsingFile, err := parser.ParseFile(tokens, "./lexicalstack/lexicalstack.go`", nil, parser.Trace)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// // Create AST (Abstract Syntax Tree) For The Module Declaration and then convert into Bytecode for more flexibilty
+	// fmt.Println(*parsingFile)
+	// // for i := 0; i < len(parsingFile.Imports); i++ {
+	// // 	fmt.Println(*parsingFile.Imports[i])
+	// // }
+	// fmt.Println(tokens)
 }
---
+++ tokens/tokens.go
@@ -0,0 +1,78 @@
+package tokens
+
+/*
+	Token Configuration for language.
+	This file contains all of the tokens necessary for the language to work.
+	More Tokens will be added in the future for reference.
+	Things to understand here are:
+		1. A new type is created here of string so that there is no mismatch between the internal string of golang and the token created for our pookie-lang.
+		2. All the tokens are converted into Hashmap for better finding of tokens when necessary.
+		3. Function lookup for identifier returns type identifier if not found in the basic tokenization.
+*/
+
+type TokenType string
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	FLOAT     = "FLOAT"
+	STRING    = "STRING"
+	BANG      = "!"
+	ASSIGN    = "="
+	PLUS      = "+"
+	MINUS     = "-"
+	ASTARISK  = "*"
+	SLASH     = "/"
+	LT        = "<"
+	GT        = ">"
+	LE        = "<="
+	GE        = ">="
+	EQ        = "=="
+	NEQ       = "!="
+	AND       = "&&"
+	OR        = "||"
+	COMMA     = ","
+	SEMICOLON = ";"
+	COLON     = ":"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	LBRACKET  = "["
+	RBRACKET  = "]"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	NIL       = "NIL"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	RETURN    = "RETURN"
+	MACRO     = "MACRO"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"nil":    NIL,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"macro":  MACRO,
+}
+
+func LookupForIdentifiers(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
