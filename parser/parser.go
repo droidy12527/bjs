af38a841a1222e37b3da2b9f4f49e7daecc9e94b
droidy12527 <gundiaramasu@gmail.com>
2024-02-19 23:45:14 +0000

    feature: added some parsing functions to evaluate ast expressions

--- parser/parser.go
+++ parser/parser.go
@@ -6,6 +6,10 @@ import (
 	"compiler/token"
 )
 
+/*
+	Refactor functions such as expectPeek and peekTokenIs for better code simplicity
+*/
+
 type Parser struct {
 	l         lexer.Lexer
 	curToken  token.Token
@@ -25,5 +29,51 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF {
+		statement := p.parseStatement()
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+			p.nextToken()
+		}
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	statement := &ast.LetStatement{Token: p.curToken}
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	statement.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	// Skip till we see a semicolon for the statement end.
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return statement
+}
+
+func (p *Parser) peekTokenIs(t token.Type) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) curTokenIs(t token.Type) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.Type) bool {
+	return p.peekTokenIs(t)
 }
