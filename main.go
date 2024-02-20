2829f6f5d8bfd53044eacad53e0f61d8d1dd601d
droidy12527 <gundiaramasu@gmail.com>
2024-02-20 10:24:00 +0000

    uncomment: uncommented the readFile() so that pookie can be scanned from the folder

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"compiler/relp"
 	"fmt"
 	"os"
+	"strings"
 )
 
 /*
 	Use pointer parsing for getting the keywords, Makes thing more easy.
 	While loading the program, Make sure to dynamically load it rather than loading it through consuming heap memory.
 	Use lexical stack for loading the text
+	Read file for compiling.
 */
 
 func main() {
@@ -19,17 +21,19 @@ func main() {
 		fmt.Println("Welcome to pookie lang debugger, This is token debugger")
 		fmt.Println("Please type in expressions to Parse: ")
 		relp.StartRELP(os.Stdin, os.Stdout)
+	} else {
+		readFile()
 	}
 }
 
-// func readFile() {
-// 	osArgs := os.Args
-// 	if len(osArgs) < 1 {
-// 		panic("not enough arguments to compile file")
-// 	}
-// 	filename := osArgs[1]
-// 	fileExtension := strings.Split(filename, ".")[1]
-// 	if fileExtension != "pookie" {
-// 		panic("file extension is wrong, make sure you are using pookie file extension.")
-// 	}
-// }
+func readFile() {
+	osArgs := os.Args
+	if len(osArgs) < 1 {
+		panic("not enough arguments to compile file")
+	}
+	filename := osArgs[1]
+	fileExtension := strings.Split(filename, ".")[1]
+	if fileExtension != "pookie" {
+		panic("file extension is wrong, make sure you are using pookie file extension.")
+	}
+}
