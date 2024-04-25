d2e4e03016d0777478a7f013da7f395fe3bad82e
droidy12527 <gundiaramasu@gmail.com>
2024-04-25 21:41:13 +0100

    fix: added new if statement parser

--- ast/ast.go
+++ ast/ast.go
@@ -74,6 +74,18 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
@@ -163,3 +175,28 @@ func (ie *InfixExpression) String() string {
 func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string       { return b.Token.Literal }
+
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+	return out.String()
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
