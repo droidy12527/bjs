88a508ebd8613c6996f38ca486d272cb8abe0689
droidy12527 <gundiaramasu@gmail.com>
2024-08-16 12:40:48 +0100

    feature: added prefix operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,8 @@ import (
 	"compiler/object"
 )
 
+// This is predeclared variable block for memory allocation for true, false and null objects in memory
+// as all of them are common and we do not need to create new memory allocation everytime
 var (
 	NULL  = &object.Null{}
 	TRUE  = &object.Boolean{Value: true}
@@ -25,8 +27,38 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBooleanToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 	return nil
+}
+
+// check for prefix operator and then check for what to do next
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// This function inverts the bang operator by sending back object
+// Basically we check for ! and if we get the prefix then we invert back the results
+// The results are then inverted and sent, Mainly this is used because we cannot just send back boolean
+// We need to send back the address of the boolean we created in memory. This makes the things faster
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return FALSE
+	default:
+		return FALSE
+	}
 }
 
 // This function is optimisation to return back the value of boolean
