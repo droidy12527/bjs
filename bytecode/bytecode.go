72cdaa227750cf5c0f464f276e37f19cfd37f01b
droidy12527 <gundiaramasu@gmail.com>
2025-01-26 20:38:53 +0000

    feature: Added making of instructions

--- bytecode/bytecode.go
+++ bytecode/bytecode.go
@@ -1,6 +1,9 @@
 package bytecode
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 // This package has all of the bytecode we require for compiling the language.
 // We will use Stack Compiling, Not using physical register as we don't have access to them
@@ -36,4 +39,34 @@ func Lookup(op byte) (*Defination, error) {
 		return nil, fmt.Errorf("opcode %d is undefined", op)
 	}
 	return def, nil
+}
+
+// Creates an slice of byte which has opcode and operand and returns it back
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definations[op]
+	if !ok {
+		return []byte{}
+	}
+	// Define instruction lenght for the CPU
+	instructionLen := 1
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+
+	// The first instruction byte should always be OpCode
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	// Define OffSet and start pushing instructions
+	offset := 1
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+	return instruction
 }
