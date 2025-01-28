d2442abc1a1150ae59c675d6efc313cff9cadddd
droidy12527 <gundiaramasu@gmail.com>
2025-01-28 17:12:25 +0000

    feature: virtual machine started

--- virtualmachine/virtualmachine.go
+++ virtualmachine/virtualmachine.go
@@ -4,6 +4,7 @@ import (
 	"compiler/code"
 	"compiler/compiler"
 	"compiler/object"
+	"fmt"
 )
 
 // Defining stacksize to also check with stack overflow
@@ -27,4 +28,41 @@ func New(bytecode *compiler.ByteCode) *VirtualMachine {
 		stack:        make([]object.Object, StackSize),
 		sp:           0,
 	}
+}
+
+// Returs back the object located on the stacktop
+func (vm *VirtualMachine) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+// Returns back error and runs the program in Fetch, Decode, Execute cycle
+func (vm *VirtualMachine) Run() error {
+	// Setup instruction pointer and start going through the pointer
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+		// Get the opcode and then start decoding in execute cycle
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// Pushes the object to stack of Virtual machine and increments the stackpointer
+func (vm *VirtualMachine) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+	vm.stack[vm.sp] = o
+	vm.sp++
+	return nil
 }
